099c70617d71b420d5c7f725376749b0e41443a7
MohitMourya <42367406+MohitMourya@users.noreply.github.com>
2020-07-23 18:08:02 +0530

    Add files via upload

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Mohit Mourya on 23/07/20.
+//  Copyright © 2020 Mohit Mourya. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+
+}
+
